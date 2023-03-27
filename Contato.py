5bd04407d6d1577d79053ea5719abd581876343a
Erika Gascao Nascimento <egnascimento@mongeralaegon.com.br>
2023-03-27 10:46:53 -0300

    criando modelos: contato

---
+++ Contato.py
@@ -0,0 +1,7 @@
+class contato:
+
+    def __init__(self,celular, telefone_residencial, telefone_comercial, email):
+        self.celular= celular
+        self.telefone_residencial = telefone_residencial
+        self.telefone_comercial = telefone_comercial
+        self.email = email
